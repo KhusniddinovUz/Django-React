9699224bb533b182fbd372ce392179439acf4329
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-14 20:48:12 +0500

    Create CRUD  API for leads

--- leadmanager/backend/leadmanager/leadmanager/settings.py
+++ leadmanager/backend/leadmanager/leadmanager/settings.py
@@ -63,6 +63,8 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'leadmanager',
         'USER': 'postgres',

13f45e95fddaaf4418743639ea216506813e33b0
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-14 21:59:38 +0500

    Connect Bootstrap and create templates

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Leads from './Leads';
+import Form from './Form';
+
+const Dashboard = () => {
+  return (
+    <div className='Dashboard'>
+      <Leads />
+      <Form />
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div className='Form'>
+      <h1>Add Lead Form</h1>
+    </div>
+  );
+};
+
+export default Form;

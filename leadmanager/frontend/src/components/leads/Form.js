f1906bc95be4f7e3bdd8a3b3e4e2147cf2c86d62
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-15 21:22:46 +0500

    Create Form and handle data

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const { name, email, message } = user;
+
+  const onChange = (e) => {
+    const newuser = Object.assign({}, user, {
+      [e.target.name]: e.target.value,
+    });
+    setUser(newuser);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    setUser({
+      name: '',
+      email: '',
+      message: '',
+    });
+  };
+
   return (
-    <div className='Form'>
-      <h1>Add Lead Form</h1>
+    <div className='Form card card-body mt-4 mb-4'>
+      <h2>Add Lead</h2>
+      <form onSubmit={onSubmit}>
+        <div className='form-group mt-2'>
+          <label>Name</label>
+          <input
+            className='form-control'
+            type='text'
+            name='name'
+            onChange={onChange}
+            value={name}
+          />
+        </div>
+        <div className='form-group mt-2'>
+          <label>Email</label>
+          <input
+            className='form-control'
+            type='email'
+            name='email'
+            onChange={onChange}
+            value={email}
+          />
+        </div>
+        <div className='form-group mt-2'>
+          <label>Message</label>
+          <textarea
+            className='form-control'
+            type='text'
+            name='message'
+            onChange={onChange}
+            value={message}
+          />
+        </div>
+        <div className='form-group'>
+          <button type='submit' className='mt-3 btn btn-primary'>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };

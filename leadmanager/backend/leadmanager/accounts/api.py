1b4bbfc2a3a5f5ec92cf46d0fca92a7cf85338a0
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-17 16:40:45 +0500

    Create LoginAPI

--- leadmanager/backend/leadmanager/accounts/api.py
+++ leadmanager/backend/leadmanager/accounts/api.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
 
 
 # Register API
@@ -16,6 +16,18 @@ class RegisterAPI(generics.GenericAPIView):
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
         })
-# Login API
 
+
+# Login API
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            'token': AuthToken.objects.create(user)[1]
+        })
 # Get User API

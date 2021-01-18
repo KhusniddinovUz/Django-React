10f45fe6ffe34e110617b22f91731df6e1327a12
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-18 16:08:32 +0500

    Login page

--- twitter-clone/frontend/src/App.js
+++ twitter-clone/frontend/src/App.js
@@ -1,9 +1,11 @@
+import Login from './components/Login';
 import Signup from './components/Signup';
 
 function App() {
   return (
     <div className='App'>
-      <Signup></Signup>
+      <Signup />
+      <Login />
     </div>
   );
 }
---
+++ twitter-clone/frontend/src/components/Login.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [user, setUser] = useState({ username: '', password: '' });
+
+  const changeHandler = (e) => {
+    const newuser = Object.assign({}, user, { [e.target.id]: e.target.value });
+    setUser(newuser);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //Fetch API for login
+    console.log(user);
+  };
+
+  return (
+    <div className='container SignUp p-3'>
+      <h1 className='mt-4 p-1 text-center'>Login page</h1>
+      <form onSubmit={submitHandler}>
+        <div className='form-group p-2 '>
+          <label htmlFor='username'>Username</label>
+          <input
+            type='text'
+            className='form-control'
+            id='username'
+            placeholder='Enter Username'
+            onChange={changeHandler}
+          />
+        </div>
+
+        <div className='form-group p-2'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            placeholder='Password'
+            onChange={changeHandler}
+          />
+        </div>
+        <div className='container d-flex align-items-center mt-2'>
+          <button type='submit' className='btn btn-primary'>
+            Login
+          </button>
+          <div className='text-center w-100'>
+            Don't have an account? <a to='/signup'>Signup</a> here
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

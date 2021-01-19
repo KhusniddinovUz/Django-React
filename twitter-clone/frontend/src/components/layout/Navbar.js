385d4a03a25f8f90251ef1512c9e07e326022527
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-19 16:16:00 +0500

    Style Navbar and TweetHome

--- twitter-clone/frontend/src/components/layout/Navbar.js
+++ twitter-clone/frontend/src/components/layout/Navbar.js
@@ -3,42 +3,42 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className='Navbar'>
-      <ul className='list-group'>
+      <ul className='list-group w-75 mx-auto'>
         <div className='list-group-item'>
           <i className='fab fa-twitter' id='twitter' />
         </div>
         <div className='list-group-item'>
-          <li className=' active-nav'>
+          <li className='active-nav d-flex align-items-center'>
             <i className='fas fa-home' />
-            Home
+            <span>Home</span>
           </li>
         </div>
         <div className='list-group-item'>
-          <li>
+          <li className='d-flex align-items-center'>
             <i className='far fa-bell' />
-            Notifications
+            <span>Notifications</span>
           </li>
         </div>
         <div className='list-group-item'>
-          <li>
+          <li className='d-flex align-items-center'>
             <i className='far fa-envelope' />
-            Messages
+            <span>Messages</span>
           </li>
         </div>
         <div className='list-group-item'>
-          <li>
+          <li className='d-flex align-items-center'>
             <i className='far fa-user' />
-            Profile
+            <span>Profile</span>
           </li>
         </div>
         <div className='list-group-item'>
-          <li>
+          <li className='d-flex align-items-center'>
             <i className='fas fa-bars' />
             <span>More</span>
           </li>
         </div>
         <div className='list-group-item'>
-          <button className='btn'>Tweet</button>
+          <button className='btn butn p-3'>Tweet</button>
         </div>
       </ul>
     </div>
--- twitter-clone/frontend/src/components/layout/TweetHome.js
+++ twitter-clone/frontend/src/components/layout/TweetHome.js
@@ -5,9 +5,12 @@ const TweetHome = () => {
   return (
     <div className='TweetHome'>
       <div>Home</div>
-      <form>
-        <img src={image} className='profile-image' />
-        <textarea type='text' placeholder="What's happening?" />
+      <form className='p-4'>
+        <div className='container d-flex justify-content-center'>
+          <img src={image} className='profile-image' />
+          <textarea type='text' placeholder="What's happening?" />
+        </div>
+        <button className='btn butn mx-auto'>Tweet</button>
       </form>
     </div>
   );

80f8bd08a6a56ad66e7bf4c4953fce87ed482031
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-19 06:29:12 +0500

    Create Navbar and its styling

--- twitter-clone/frontend/src/components/layout/Navbar.js
+++ twitter-clone/frontend/src/components/layout/Navbar.js
@@ -2,13 +2,44 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='Navbar d-flex'>
-      <ul>
-        <li>Home</li>
-        <li>Notifications</li>
-        <li>Messages</li>
-        <li>Profile</li>
-        <li>More</li>
+    <div className='Navbar'>
+      <ul className='list-group'>
+        <div className='list-group-item'>
+          <i className='fab fa-twitter' id='twitter' />
+        </div>
+        <div className='list-group-item'>
+          <li className=' active-nav'>
+            <i className='fas fa-home' />
+            Home
+          </li>
+        </div>
+        <div className='list-group-item'>
+          <li className=''>
+            <i className='far fa-bell' />
+            Notifications
+          </li>
+        </div>
+        <div className='list-group-item'>
+          <li className=''>
+            <i className='far fa-envelope' />
+            Messages
+          </li>
+        </div>
+        <div className='list-group-item'>
+          <li className=''>
+            <i className='far fa-user' />
+            Profile
+          </li>
+        </div>
+        <div className='list-group-item'>
+          <li className=''>
+            <i className='fas fa-bars' />
+            <span>More</span>
+          </li>
+        </div>
+        <div className='list-group-item'>
+          <button className='btn'>Tweet</button>
+        </div>
       </ul>
     </div>
   );

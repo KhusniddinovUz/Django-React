027666e28882f13c4fcd949d544e0ccdfc24f9a2
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-20 11:42:04 +0500

    Create TweetModel

--- twitter-clone/backend/tweets/models.py
+++ twitter-clone/backend/tweets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Tweet Model
+class TweetModel(models.Model):
+    text = models.TextField(max_length=150, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name='account', on_delete=models.CASCADE)

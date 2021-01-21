52aef0f9add36fd41cc60971673115d1627be5bb
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-21 09:25:54 +0500

    Add username of owner to TweetModel

--- twitter-clone/backend/tweets/api.py
+++ twitter-clone/backend/tweets/api.py
@@ -15,4 +15,4 @@ class TweetViewset(viewsets.ModelViewSet):
         return ordered
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        serializer.save(owner=self.request.user, username=self.request.user.username)
--- twitter-clone/backend/tweets/models.py
+++ twitter-clone/backend/tweets/models.py
@@ -8,3 +8,4 @@ class TweetModel(models.Model):
     text = models.TextField(max_length=300, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True)
+    username = models.CharField(max_length=100, blank=True)

192c7c483ea0a4e1083c91a84a7fa6f085a358c9
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-21 06:15:11 +0500

    return sorted tweets

--- twitter-clone/backend/tweets/api.py
+++ twitter-clone/backend/tweets/api.py
@@ -11,7 +11,9 @@ class TweetViewset(viewsets.ModelViewSet):
     serializer_class = TweetSerializer
 
     def get_queryset(self):
-        return TweetModel.objects.all()
+        ordered = TweetModel.objects.order_by('-created_at')
+        return ordered
 
     def perform_create(self, serializer):
+        print(self.request.user)
         serializer.save(owner=self.request.user)

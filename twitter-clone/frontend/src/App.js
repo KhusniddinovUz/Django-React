dff6ef944e4bec4719f750daae64cd6c9e069aad
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-22 14:14:06 +0500

    Create individual Home component

--- twitter-clone/frontend/src/App.js
+++ twitter-clone/frontend/src/App.js
@@ -1,50 +1,46 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
 } from 'react-router-dom';
-import { loadUser } from './action/auth';
+import {loadUser} from './action/auth';
 import Navbar from './components/layout/Navbar';
-import TweetHome from './components/layout/TweetHome';
 import Alerts from './components/layout/Alerts';
-import Feed from './components/layout/Feed';
+import Home from "./components/NavComponents/Home";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadUser());
-  }, []);
-  const auth = useSelector((state) => state.auth);
-  return (
-    <Router>
-      <div className='App'>
-        <Alerts />
-        {!auth.isAuthenticated ? <Redirect to='/login' /> : ''}
-        <Switch>
-          <Route
-            exact
-            path='/'
-            render={() => (
-              <div className='Home'>
-                <Navbar />
-                <div className='Main'>
-                  <TweetHome />
-                  <Feed />
-                </div>
-              </div>
-            )}
-          />
-          <Route path='/register' component={Signup} />
-          <Route path='/login' component={Login} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadUser());
+    }, []);
+    const auth = useSelector((state) => state.auth);
+    return (
+        <Router>
+            <div className='App'>
+                <Alerts/>
+                {!auth.isAuthenticated ? <Redirect to='/login'/> : ''}
+                <Switch>
+                    <Route
+                        exact
+                        path='/'
+                        render={() => (
+                            <div className='Main'>
+                                <Navbar/>
+                                <Home/>
+                            </div>
+                        )}
+                    />
+                    <Route path='/register' component={Signup}/>
+                    <Route path='/login' component={Login}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;

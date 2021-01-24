3fd0b4e584e32d5c4e387e9e5ee1b4665d457d59
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-24 17:02:11 +0500

    Install and config DRF

--- blogapi/config/settings.py
+++ blogapi/config/settings.py
@@ -24,7 +24,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':
+        ['rest_framework.permissions.AllowAny']
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

c8252989dc77d6416e31a671f91be4c27aa0c1ff
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-24 15:37:05 +0500

    render dummy data

--- Todo/frontend/src/App.js
+++ Todo/frontend/src/App.js
@@ -1,7 +1,33 @@
+import { useState } from 'react';
 function App() {
+  const list = [
+    {
+      id: 1,
+      title: '1st todo',
+      body: 'Learn Django properly.',
+    },
+    {
+      id: 2,
+      title: 'Second item',
+      body: 'Learn Python.',
+    },
+    {
+      id: 3,
+      title: 'Learn HTTP',
+      body: "It's important.",
+    },
+  ];
+  const [state, setState] = useState(list);
+
   return (
     <div className='App'>
-      <h1>Hi there</h1>
+      {state &&
+        state.map((item) => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
     </div>
   );
 }

ac4caecf0c32c769cf8be666efe0ef42de4a04c0
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-25 18:12:11 +0500

    render own tweets

--- twitter-clone/frontend/src/components/layout/Navbar.js
+++ twitter-clone/frontend/src/components/layout/Navbar.js
@@ -2,37 +2,39 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const onClick = (e) => {};
+
   return (
     <div className='Navbar'>
       <ul className='list-group w-75 mx-auto'>
         <div className='list-group-item'>
           <i className='fab fa-twitter' id='twitter' />
         </div>
-        <div className='list-group-item'>
-          <li className='active-nav d-flex align-items-center'>
+        <div onClick={onClick} className='list-group-item'>
+          <li className='d-flex align-items-center'>
             <i className='fas fa-home' />
             <NavLink to='/home'>Home</NavLink>
           </li>
         </div>
-        <div className='list-group-item'>
+        <div onClick={onClick} className='list-group-item'>
           <li className='d-flex align-items-center'>
             <i className='far fa-bell' />
             <span>Notifications</span>
           </li>
         </div>
-        <div className='list-group-item'>
+        <div onClick={onClick} className='list-group-item'>
           <li className='d-flex align-items-center'>
             <i className='far fa-envelope' />
             <span>Messages</span>
           </li>
         </div>
-        <div className='list-group-item'>
+        <div onClick={onClick} className='list-group-item'>
           <li className='d-flex align-items-center'>
             <i className='far fa-user' />
             <NavLink to='/home/profile'>Profile</NavLink>
           </li>
         </div>
-        <div className='list-group-item'>
+        <div onClick={onClick} className='list-group-item'>
           <li className='d-flex align-items-center'>
             <i className='fas fa-bars' />
             <span>More</span>

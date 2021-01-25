9c23f4d54c1c79b4806f1285541483830df5a014
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-25 07:18:45 +0500

    Create user serializer, view and endpoint

--- blogapi/backend/posts/views.py
+++ blogapi/backend/posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 
@@ -14,3 +15,13 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer

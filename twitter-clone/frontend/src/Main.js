716ec6d00feb7bdad29280115b0a6d988c99ae14
KhusniddinovUz <khusniddinovuz@gmail.com>
2021-01-29 06:47:55 +0500

    Add empty component

--- twitter-clone/frontend/src/Main.js
+++ twitter-clone/frontend/src/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/NavComponents/Home';
 import Profile from './components/NavComponents/Profile';
+import Empty from './components/NavComponents/Empty';
 
 const Main = (props) => {
   return (
@@ -11,6 +12,9 @@ const Main = (props) => {
       <Switch>
         <Route exact path='/home' component={Home} />
         <Route exact path='/home/profile' component={Profile} />
+        <Route exact path='/home/notifications' component={Empty} />
+        <Route exact path='/home/messages' component={Empty} />
+        <Route exact path='/home/more' component={Empty} />
       </Switch>
     </div>
   );
